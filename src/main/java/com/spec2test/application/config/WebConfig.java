c0805e41343285aa7a37a77df764b700effcc122
K-AMeus <karl.andreas@meus.ee>
2025-05-07 22:16:48 +0300

    Change production url

--- src/main/java/com/spec2test/application/config/WebConfig.java
+++ src/main/java/com/spec2test/application/config/WebConfig.java
@@ -48,7 +48,7 @@ public class WebConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173",
-                "https://spec2testfe-production.up.railway.app"));
+                "https://reqflowly.up.railway.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
